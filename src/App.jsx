1abfc23ac2eede77169737818fa8099a0122e163
hodooniihoosonmantuu <hodooniihoosonmantuu@gmail.com>
2025-12-03 16:58:00 +0800

    Restore DepartmentsWidget with 20 departments, slider functionality, and clean minimal design

--- src/App.jsx
+++ src/App.jsx
@@ -101,12 +101,10 @@ function App() {
               <NewsSection title="Алтны тойм мэдээ" variant="gold" />
             </div>
             <div className="center-column">
+              <DepartmentsWidget />
               <NewsSection title="Аюулгүй ажиллагааны сэрэмжлүүлэг" variant="safety" />
             </div>
           </div>
-
-          {/* Departments Row */}
-          <DepartmentsWidget />
 
           {/* Applications & Weather Row */}
           <div className="apps-weather-row">
